064c745b2f1ba6376e3b1dc20bd2bfe0ad8af9b6
michaelspall <42585574+michaelspall@users.noreply.github.com>
2022-10-20 11:47:21 -0400

    Add files via upload

---
+++ MAS.h
@@ -0,0 +1,7 @@
+      common /mas/ trestore,XRW,XRE,YRS,YRN,tauspinup,tauperiod
+     .            ,XPW,XPE,YPS,YPN,mskptr,mskfrc,theta1,theta2
+     .            ,ksill,ptrestore,taumean
+       INTEGER mskfrc(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+       INTEGER mskptr(Nr),ksill
+      _RL trestore,XRW,XRE,YRS,YRN,tauspinup,XPW,XPE,YPS,YPN,tauperiod
+      _RL angl, theta1, theta2, ptrestore, taumean
